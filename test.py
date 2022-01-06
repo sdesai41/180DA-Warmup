23941f236f9c6f05400f3cd553f43892e180ec2a
sdesai41 <sohumdesai41@gmail.com>
2022-01-06 15:59:57 -0800

    test works

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 15:55:48 2022
+
+@author: sohum
+"""
+
+if __name__=="__main__":
+    x="ECE180DA/DB"
+    if x=="ECE180DA/DB":
+        print("you are living in 2017")
+    else:
+        x=x+"Best class ever"
+        print(x)
